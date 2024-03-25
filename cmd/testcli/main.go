6ffc679782738b965dad53a3c97ef15b0dfd2818
Bradford Wagner <wagner.bradford@gmail.com>
2024-03-25 14:19:26 -0400

    Initial commit

---
+++ cmd/testcli/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"template_cli/internal/log"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "testcli",
+}
+
+var myVerb = &cobra.Command{
+	Use:   "myVerb",
+	Short: "myVerb does something",
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return []string{}, cobra.ShellCompDirectiveDefault
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		l := log.Log().With("cmd", "myVerb")
+		l.Info("hi friends")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(myVerb)
+}
+
+func main() {
+	// cobra
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
